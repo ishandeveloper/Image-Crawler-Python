a67b0377d7634b5453725ce0397bce15f81e30b0
Ishan Sharma <ishandeveloper@outlook.com>
2020-03-24 09:59:21 +0530

    Added opener & headers

--- crawler.py
+++ crawler.py
@@ -4,3 +4,11 @@ import bs4
 print("IMAGE CRAWLER by ishandeveloper\n\n")
 url = str(input("Enter or Paste the URL: "))
 url.strip()
+
+try:
+    print("Connecting To The Host .... " + url)
+    opener=urllib.request.build_opener()
+    opener.add_headers=[{'User-Agent' : 'Mozilla'}]
+    urllib.request.install_opener(opener)
+
+except:
